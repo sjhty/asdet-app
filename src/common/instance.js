f9b30fa2c44b16b8791ed8dd2936b11d8b31cfb6
sjhty <sun_j1986@163.com>
2019-03-31 22:24:48 +0800

    master

--- src/common/instance.js
+++ src/common/instance.js
@@ -6,3 +6,15 @@ const instance = axios.creat({
     xsrfCookieName: "XSRF-TOKEN",
     baseUrl: baseUrl,
 })
+
+//响应数据拦截器
+instance.interceptors.response.use(function(response){
+    if (response.data.success) {
+        return Promise.resolve(response.data);
+    } else {
+        //弹出信息
+        return Promise.reject({
+            message: response.data.message
+        })
+    }
+})
